1fb5f31f994d9c951a05ccf1ff31f3550f88f8ef
agranosik <agranosik@r.pl>
2022-03-01 13:31:39 +0100

    input validation

--- src/Tournament/Create/create.tsx
+++ src/Tournament/Create/create.tsx
@@ -1,22 +1,31 @@
+import { ErrorMessage } from '@hookform/error-message';
 import React, { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form';
 import './create.css'
+
+interface Inputs {
+    name: string,
+    numberOfPLayers: number
+}
+
 
 export default function CreateTournament() {
 
-    const [name, setName] = useState('');
-    const [numberOfPlayers, setNumberOfPlayers] = useState(0);
+    const { register, handleSubmit, watch, formState: { errors }} = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log('hehehehe');
-    }
+    console.log(watch('numberOfPLayers'), errors);
+
         return (
             <div className='tournament-container'>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <label>Nazwa</label>
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
-                    <label>Nazwa</label>
-                    <input type="text" value={numberOfPlayers} onChange={(e) => setNumberOfPlayers(e.target.value)}></input>
+                    <input type="text" {...register('name', { required: 'This is ...', maxLength: 50 })}></input>
+                    <ErrorMessage errors={errors} name="name" />
+                    <label>Liczba graczy</label>
+                    <input type="text" {...register('numberOfPLayers', { required: true, min: 1, max: {value: 20, message: 'Maksymalna wartość wynosi: 20'}})}></input>
+                    <ErrorMessage errors={errors} name="numberOfPLayers" />
+
                     <input type="submit" value="Stwórz" />
                 </form>
             </div>
