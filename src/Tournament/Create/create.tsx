04ab63f5cabaa52d456a36ff0166d7da377fd211
Granosik Adrian <adrian.granosik@gmail.com>
2022-03-03 07:57:41 +0100

    styles

--- src/Tournament/Create/create.tsx
+++ src/Tournament/Create/create.tsx
@@ -18,14 +18,20 @@ export default function CreateTournament() {
         return (
             <div className='tournament-container'>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <label>Nazwa</label>
-                    <input type="text" {...register('name', { required: 'This is ...', maxLength: 50 })}></input>
-                    <ErrorMessage errors={errors} name="name" />
-                    <label>Liczba graczy</label>
-                    <input type="text" {...register('numberOfPlayers', { required: true, min: 1, max: {value: 20, message: 'Maksymalna wartość wynosi: 20'}})}></input>
-                    <ErrorMessage errors={errors} name="numberOfPlayers" />
+                    <div className='input-container name-container'>
+                        <label>Nazwa</label>
+                        <input type="text" {...register('name', { required: 'This is ...', maxLength: 50 })}></input>
+                        <ErrorMessage errors={errors} name="name" />
+                    </div>
+                    <div className='input-container numberOfPlayers-container'>
+                        <label>Liczba graczy</label>
+                        <input type="text" {...register('numberOfPlayers', { required: true, min: 1, max: {value: 20, message: 'Maksymalna wartość wynosi: 20'}})}></input>
+                        <ErrorMessage errors={errors} name="numberOfPlayers" />
 
-                    <input type="submit" value="Stwórz" />
+                    </div>
+                    <div className='submit-container'>
+                        <input type="submit" value="Stwórz" />
+                    </div>
                 </form>
             </div>
         );
