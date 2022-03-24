504b8e95173961f0038fa0a9c25d99e3ea8a4f8b
Granosik Adrian <adrian.granosik@gmail.com>
2022-03-24 07:55:22 +0100

    singlar problems unresolved

--- src/Tournament/Managment/tournamentManagement.tsx
+++ src/Tournament/Managment/tournamentManagement.tsx
@@ -1,22 +1,44 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { Store } from '../../redux/store';
 import { getTournamentAsyncThunk } from '../../redux/tournament/tournamentSlice.tsx';
-import './tournamentManagement.scss'
+import './tournamentManagement.scss';
+import { HubConnectionBuilder } from '@microsoft/signalr';
 
 export default function TournamentManagement(){
 
+    const [connection, setConnection] = useState(null);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const newConnection = new HubConnectionBuilder()
+            .withUrl('https://localhost:7008/hubs/player')
+            .withAutomaticReconnect()
+            .build();
+
+        setConnection(newConnection);
+        dispatch(getTournamentAsyncThunk(id));
+    }, []);
+
+    useEffect(() => {
+        if (connection) {
+            connection.start()
+                .then(result => {
+                    console.log('Connected!');
+    
+                    connection.on('Test', message => {
+                        alert(message);
+                    });
+                })
+                .catch(e => console.log('Connection failed: ', e));
+        }
+    }, [connection]);
+
     const {id} = useParams();
     const tournament = useSelector((state: Store) => {
-        console.log(state);
-        console.log('hehere')
         return state.tournament
     });
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(getTournamentAsyncThunk(id));
-    }, [])
 
     return(
         <div>
@@ -24,6 +46,7 @@ export default function TournamentManagement(){
                 <img src={`data:image/jpeg;base64,${tournament.qrCode}`} />
             </div>
             <div>Managment</div>
+            <button onClick={() => connection.send('Test', 'fiu fiu')}>Button</button>
         </div>
     );
 }
