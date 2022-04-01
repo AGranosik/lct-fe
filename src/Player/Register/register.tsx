311341dca0fa52e6f3d18100a57ac1b769dd473a
Granosik Adrian <adrian.granosik@gmail.com>
2022-04-01 09:57:42 +0200

    submit method

--- src/Player/Register/register.tsx
+++ src/Player/Register/register.tsx
@@ -1,28 +1,30 @@
 import { ErrorMessage } from "@hookform/error-message";
 import { Button, TextField } from "@mui/material";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { PlayerRegisterModel } from "./Models/playerRegisterModel.tsx";
-
+import './register.scss'
 export default function PlayerRegister(){
     const { register, handleSubmit, formState: { errors }} = useForm<PlayerRegisterModel>(); 
+    const onSubmit: SubmitHandler<PlayerRegisterModel> = (data: PlayerRegisterModel) => console.log(data);
     return(
         <div className="registration-container">
-            <div className='input-container name-container'>
-                <TextField error={!!errors.name} fullWidth label="Imie" {...register('name', { required: 'This is ...', maxLength: 50 })} />
-                <div className='error-container'>
-                    <ErrorMessage errors={errors} name="name" />
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className='input-container name-container'>
+                    <TextField error={!!errors.name} fullWidth label="Imie" {...register('name', { required: 'This is ...', maxLength: 50 })} />
+                    <div className='error-container'>
+                        <ErrorMessage errors={errors} name="name" />
+                    </div>
                 </div>
-            </div>
-            <div className='input-container surname-container'>
-                <TextField error={!!errors.surname} fullWidth label="Liczba graczy" {...register('surname', { required: 'This is ...', maxLength: 50 })} />
-                <div className='error-container'>
-                    <ErrorMessage errors={errors} name="surname" />
+                <div className='input-container surname-container'>
+                    <TextField error={!!errors.surname} fullWidth label="Nazwisko" {...register('surname', { required: 'This is ...', maxLength: 50 })} />
+                    <div className='error-container'>
+                        <ErrorMessage errors={errors} name="surname" />
+                    </div>
                 </div>
-            </div>
-            <div className='submit-container'>
-                <Button type="submit" variant="contained">Stwórz</Button>
-            </div>
-
+                <div className='submit-container'>
+                    <Button type="submit" variant="contained">Dołącz</Button>
+                </div>
+            </form>
         </div>
     )
 }
