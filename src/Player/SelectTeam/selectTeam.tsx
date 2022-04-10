f8c8d71d8e33d8617990109a2169a28d9af4fa0b
Granosik Adrian <adrian.granosik@gmail.com>
2022-04-10 22:21:25 +0200

    select team page

--- src/Player/SelectTeam/selectTeam.tsx
+++ src/Player/SelectTeam/selectTeam.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { getTeamsApi } from "../../api/Team/teamApi.tsx";
 import { Store } from "../../redux/store.tsx";
 import { getTeamsAsyncThunk } from "../../redux/team/teamSlice.tsx";
+import './selectTeam.scss'
 
 export default function SelectTeam() {
     const {id} = useParams();
@@ -15,10 +16,20 @@ export default function SelectTeam() {
         dispatch(getTeamsAsyncThunk());
     }, [])
 
+    const selectTeam = () => {
+        return teams.map((team: string) => {
+            return (<div key={team} className="team">{team}</div>)
+        })
+    } 
     return(
-        <div>
-            <div>Select</div>
-            {teams.length}
+        <div className="team-container">
+            <div className="title">Wybierz drużynę</div>
+            <div className="teams-container">
+                {selectTeam()}
+            </div>
+            <div className="team-select-button">
+                <Button variant="contained">Wybierz</Button>
+            </div>
         </div>
     )
 }
