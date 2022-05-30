12479dddf8e24b330e6164b5f62f6d85080bdb62
Granosik Adrian <adrian.granosik@gmail.com>
2022-05-30 16:09:31 +0200

    team selected page

--- src/Player/TeamSelected/teamSelected.tsx
+++ src/Player/TeamSelected/teamSelected.tsx
@@ -3,6 +3,6 @@ import React from 'react'
 
 export default function SelectedTeam () {
     return (
-        <div>hehe</div>
+        <div className='team-selected'>Oczekuj na pozostałych</div>
     )
 }
